678bdbbbd72417ee407cbff04f6f4f1613626c4e
apkmodify <102298294+apkmodify@users.noreply.github.com>
2022-10-10 12:46:48 +0700

    Update games.js

--- app/js/games.js
+++ app/js/games.js
@@ -7,9 +7,9 @@ gmod.onload = function(){
 		let output = "";
 		for(let item of games){
 			output += `
-<article>
+<article class="swiper-slide">
 <a href="${item.link}" class="shadow-0">
-  <div class="item swiper-slide draggable">
+  <div class="item draggable">
     <div class="card hover-shadow" style="max-width: 7em; min-width: 7em; min-height: 12em; max-height: 12em">
       <img src="${item.images}" class="card-img-top" alt="${item.alt}"/>
         <span class="badge fs-badge rounded-pill fw-bold bg-danger position-absolute shadow-2 mt-1 ms-1 top-0 start-0">${item.badge}</span>
